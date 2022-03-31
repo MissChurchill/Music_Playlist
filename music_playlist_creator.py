1960afce5b920e4b8e51ff2cf97b44aad9a56387
Domonique Churchill <85803952+MissChurchill@users.noreply.github.com>
2022-03-31 19:53:57 -0400

    Update to music_playlist_creator.py
    Updates to file working continuous loop and pandas import

--- music_playlist_creator.py
+++ music_playlist_creator.py
@@ -1,14 +1,19 @@
 import requests
 import json
+import pandas as pd
 
-#API source: https://itunes.apple.com
 print("Welcome! You can use this application to create a music playlist")  
 
 while True:
     song=input("What song would you like to add to your playlist?")
-    response = requests.get('https://itunes.apple.com/search?term='+song+"&limit=1")
-    jsonResponse=response.json()
+    
+    if (song.lower() == "quit"):
+        print("Thank you for creating your playlist, Enjoy!")
+        break # User said to quit in any case; exit the loop
 
+    response = requests.get("https://itunes.apple.com/search?term="+song+"&limit=1")
+    jsonResponse=response.json()
+    
     for results in jsonResponse['results']:
         print("Artist Name is")
         print(results['artistName'])
@@ -22,10 +27,11 @@ while True:
         print(results['artworkUrl30'])
         print("Here is the link to the song")
         print(results['trackViewUrl'])
-            
-music_array = []
-music_array.append(results['trackViewUrl'])
 
-if input == q:
-    print ("Thank you for using our app. Enjoy your music playlist")
-    exit()
+    playlist ={
+        "song_name": str((results['trackName'])),
+        "musician": str((results['artistName'])),
+        "recording":str((results['trackViewUrl'])) }    
+    
+df = pd.DataFrame(playlist, columns = ['Musician', 'Song Title', 'Link to Song'])
+print(df)
